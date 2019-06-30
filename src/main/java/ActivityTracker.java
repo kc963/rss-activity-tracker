8f62075dbbb1f997827c4c9c0aa92124600a6528
Kapil Chopra <kapilchopra963@gmail.com>
2019-06-30 19:32:41 -0400

    Refactored code and handled exceptions.

--- src/main/java/ActivityTracker.java
+++ src/main/java/ActivityTracker.java
@@ -1,19 +1,46 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.stream.XMLStreamException;
 
 public class ActivityTracker {
 
+	private static List<String> getInactiveFeeds(HashMap<String, List<String>> companyFeedMap, int days) throws XMLStreamException, javax.xml.stream.FactoryConfigurationError {
+		List<String> inactiveFeeds = new LinkedList<String>();
+		
+		for(Map.Entry<String, List<String>> entry: companyFeedMap.entrySet()) {
+			for(String feedURL : entry.getValue()) {
+				LocalDate feedDate = RSSFeedParser.getInstance().getUpdateDate(feedURL);
+				if(feedDate == null) {
+					continue;
+				}
+				LocalDate currentDate = LocalDate.now();
+				if( currentDate.minusDays(days).compareTo(feedDate) > 0) {
+					inactiveFeeds.add(entry.getKey());
+				}
+			}
+		}
+		
+		return inactiveFeeds;
+	}
+	
 	@SuppressWarnings("restriction")
 	public static void main(String[] args) throws FactoryConfigurationError, XMLStreamException, javax.xml.stream.FactoryConfigurationError {
 		// TODO Auto-generated method stub
 
-		String feedURL = 
-				//"https://www.bbc.com/news/10628494";
-				//"https://www.craigslist.org/about/rss";
-				"https://www.craigslist.org/about/best/all/index.rss";
-				//"https://www.espn.com/espn/rss/news";
-		//String feedURL = "https://www.vogella.com/article.rss";
-		System.out.println(RSSFeedParser.getInstance().getUpdateDate(feedURL));
+		HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+		map.put("BBC", new LinkedList<String>(Arrays.asList( "http://feeds.bbci.co.uk/news/rss.xml?edition=us", "http://feeds.bbci.co.uk/news/rss.xml?edition=uk", "http://feeds.bbci.co.uk/news/rss.xml?edition=int" )));
+		map.put("Real Time with Bill Maher", new LinkedList<String>(Arrays.asList( "http://billmaher.hbo.libsynpro.com/rss" )));
+		map.put("Bill Simmons Podcast", new LinkedList<String>(Arrays.asList( "https://rss.art19.com/the-bill-simmons-podcast" )));
+		map.put("Craigslist", new LinkedList<String>(Arrays.asList( "https://www.craigslist.org/about/best/all/index.rss" )));
+		map.put("ESPN", new LinkedList<String>(Arrays.asList( "https://www.espn.com/espn/rss/news", "https://www.espn.com/espn/rss/nfl/news", "https://www.espn.com/espn/rss/nba/news" )));
+		
+		System.out.println(getInactiveFeeds(map, 1));
 		
 	}
 
--- src/main/java/RSSFeedParser.java
+++ src/main/java/RSSFeedParser.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.time.LocalDate;
 
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLEventReader;
@@ -9,70 +10,110 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.XMLEvent;
 
-public class RSSFeedParser implements Cloneable{
+public class RSSFeedParser implements Cloneable {
 
 	private static RSSFeedParser parser;
-	
-	private RSSFeedParser() {};
-	
+	private String[] monthNames;
+
+	private RSSFeedParser() {
+		monthNames = new String[] { "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov",
+				"dec" };
+	}
+
 	public static RSSFeedParser getInstance() {
-		if(parser == null) {
+		if (parser == null) {
 			parser = new RSSFeedParser();
 		}
 		return parser;
 	}
-	
+
 	@SuppressWarnings("restriction")
-	public String getUpdateDate(String feedURL) throws XMLStreamException, FactoryConfigurationError {
-		String updateDate = "";
-		
-		InputStream inputStream = connectToFeedServer(feedURL);
-		
-		XMLEventReader eventReader = XMLInputFactory.newInstance().createXMLEventReader(inputStream);
-		
-		while(eventReader.hasNext()) {
+	public LocalDate getUpdateDate(String feedURL) throws XMLStreamException, FactoryConfigurationError {
+		InputStream feedStream = connectToFeedServer(feedURL);
+		if (feedStream == null) {
+			return null;
+		}
+		XMLEventReader eventReader = XMLInputFactory.newInstance().createXMLEventReader(feedStream);
+		LocalDate date = getDate(eventReader);
+		if (date == null) {
+			System.out.println(
+					"ERROR : The Date Format in the feed is not supported by this program. Please refer to the Readme.");
+		}
+		return date;
+	}
+
+	private InputStream connectToFeedServer(String feedURL) {
+		InputStream stream = null;
+		try {
+			URL url = new URL(feedURL);
+			stream = url.openStream();
+		} catch (IOException e) {
+			System.out.println("ERROR : Couldn't connect to the feed server.");
+		}
+		return stream;
+	}
+
+	private LocalDate getDate(XMLEventReader eventReader) throws XMLStreamException {
+		LocalDate date = null;
+		while (eventReader.hasNext()) {
 			XMLEvent event = eventReader.nextEvent();
-			
-			if(event.isStartElement()) {
-				 String feedItem = event.asStartElement().getName().getLocalPart();
-                 if(feedItem.toLowerCase().indexOf("date") > -1) {
-                 	updateDate = getCharacterData(event, eventReader);
-                 	if(updateDate.length() > 7) {
-                 		return updateDate;
-                 	}
-                 }
+			if (event.isStartElement()) {
+				String feedItem = event.asStartElement().getName().getLocalPart();
+				if (feedItem.toLowerCase().indexOf("date") > -1) {
+					try {
+						date = parseDate(getDateString(event, eventReader));
+					} catch (Exception e) {
+						continue;
+					}
+					return date;
+				}
 			}
 		}
-		
-		return updateDate;
-		
+		return date;
 	}
-	
-	private InputStream connectToFeedServer(String feedURL) {
-		URL url;
-		InputStream stream;
-		try {
-			url = new URL(feedURL);
-            stream = url.openStream();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-		return stream;
-    }
-	
+
 	@SuppressWarnings("restriction")
-	private String getCharacterData(XMLEvent event, XMLEventReader eventReader) throws XMLStreamException {
-        String result = "";
-        event = eventReader.nextEvent();
-        if (event instanceof Characters) {
-            result = event.asCharacters().getData();
-        }
-        return result;
-    }
-	
-	
+	private String getDateString(XMLEvent event, XMLEventReader eventReader) throws XMLStreamException {
+		String dateString = "";
+		event = eventReader.nextEvent();
+		if (event instanceof Characters) {
+			dateString = event.asCharacters().getData();
+		}
+		return dateString;
+	}
+
+	/**
+	 * @param dateString
+	 * @return LocalDate object. (YYYY-MM-DD)
+	 * @description Parses 2 date formats: (1) Day, DD MMM YYYY (2) YYYY-MM-DD and
+	 *              generates an object.
+	 */
+	private LocalDate parseDate(String dateString) {
+		String DD = "", MM = "", YYYY = "";
+		if (dateString.indexOf(',') < 5 && dateString.indexOf(',') > -1) {
+			// process format "Day, DD MON YYYY" (Example : Sat, 10 Jun 2019)
+			DD = dateString.substring(5, 7);
+			MM = dateString.substring(8, 11).toLowerCase();
+			for (int i = 0; i < monthNames.length; i++) {
+				MM = monthNames[i].equals(MM) ? "" + (i + 1) : MM;
+			}
+			if (MM.length() < 2) {
+				MM = "0" + MM;
+			}
+			YYYY = dateString.substring(12, 16);
+		} else if (dateString.indexOf('-') > -1) {
+			// process format "YYYY-MM-DD" (Example : 2019-05-31)
+			DD = dateString.substring(8, 10);
+			MM = dateString.substring(5, 7);
+			YYYY = dateString.substring(0, 4);
+		}
+		dateString = YYYY + "-" + MM + "-" + DD;
+		LocalDate date = LocalDate.parse(dateString);
+		return date;
+	}
+
 	protected Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
 	}
-	
+
 }
